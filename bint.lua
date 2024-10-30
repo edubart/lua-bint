76b4cfc82749f7f23a801844b1e14ed636936e29
Eduardo Bart <edub4rt@gmail.com>
2024-10-30 08:37:48 -0300

    Add license to library file

--- bint.lua
+++ bint.lua
@@ -93,6 +93,10 @@ will throw assertions.
 * For doing power operation preserving integers use the @{bint.ipow} function.
 * Configure the proper integer size you intend to work with, otherwise large integers may wrap around.
 
+## License
+
+MIT, see end of file.
+
 ]]
 
 -- Returns number of bits of the internal lua integer type.
@@ -1737,3 +1741,27 @@ return bint
 end
 
 return newmodule
+
+--[[
+The MIT License (MIT)
+
+Copyright (c) 2020-2024 Eduardo Bart (https://github.com/edubart)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+]]
